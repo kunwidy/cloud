9fd9b3f8369057b9e22aece57ffb477f510aa175
kunwidy <kunwidy@gmail.com>
2021-08-04 18:05:52 +0700

    add domain templates

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,41 @@
 //main.js file
+
+//Unlock alert
+function sweetUnlock (){
+    swal({
+      title: "Unlock Domain?",
+      text: "Domain ini akan di unlock, Apakah Anda yakin?",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    })
+    .then((willDelete) => {
+      if (willDelete) {
+        swal("Selamat! domain berhasil diunlock!", {
+          icon: "success",
+        });
+      } else {
+        swal("Cancle", "Domain batal diunlock!");
+      }
+    });
+  }
+
+//Unlock alert
+function sweetLocked (){
+  swal({
+    title: "Lock Domain?",
+    text: "Domain ini akan di lock, Apakah Anda yakin?",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      swal("Selamat! domain berhasil dilock!", {
+        icon: "success",
+      });
+    } else {
+      swal("Cancle", "Domain batal dilock!");
+    }
+  });
+}
